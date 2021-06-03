715986f73e3f58f05c06c2df8e0ba4b898f89b99
hunter britten <huntergbritten@gmail.com>
2021-06-03 10:22:31 -0700

    prepare for stretch

--- python/code_challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ python/code_challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -32,60 +32,60 @@ class Queue:
             raise InvalidOperationError("Method not allowed on empty collection")
 
 
-# class AnimalShelter:
-#     def __init__(self):
-#         self.shelter = Queue()
-
-#     # ToDO
-#     # enqueue and dequeue methods
-#     # preferred method for pref cat or dog that returns null if none is selected
-
-#     def enqueue(self, pet):
-#         """Enqueue for AnimalShelter class requires a pet to be passed in"""
-#         self.shelter.enqueue(pet)
-
-#     def dequeue(self, pref):
-#         if not pref in ["dog", "cat"]:
-#             return None
-#         pet = None
-#         front = None
-#         current = self.shelter.dequeue()
-#         while current != front:
-#             front = front or current
-#             if self.pref_check(pref, current):
-#                 if current == front:
-#                     return current
-#                 pet = pet or current
-#             self.shelter.enqueue(current)
-#             current = self.shelter.dequeue()
-#         return pet
-
-#     def pref_check(self, pref, pet):
-#         if pref == "cat":
-#             thing_to_dequeue = Cat
-#         elif pref == "dog":
-#             thing_to_dequeue = Dog
-#         return isinstance(thing_to_dequeue, object)
-
-
 class AnimalShelter:
     def __init__(self):
-        self.cats = Queue()
-        self.dogs = Queue()
+        self.shelter = Queue()
 
-    def enqueue(self, animal):
-        if isinstance(animal, Dog):
-            self.dogs.enqueue(animal)
-        else:
-            self.cats.enqueue(animal)
+    # ToDO
+    # enqueue and dequeue methods
+    # preferred method for pref cat or dog that returns null if none is selected
+
+    def enqueue(self, pet):
+        """Enqueue for AnimalShelter class requires a pet to be passed in"""
+        self.shelter.enqueue(pet)
 
     def dequeue(self, pref):
-        if pref == "dog" and self.dogs:
-            return self.dogs.dequeue()
-        if pref == "cat" and self.cats:
-            return self.cats.dequeue()
+        if not pref in ["dog", "cat"]:
+            return None
+        pet = None
+        front = None
+        current = self.shelter.dequeue()
+        while current != front:
+            front = front or current
+            if self.pref_check(pref, current):
+                if current == front:
+                    return current
+                pet = pet or current
+            self.shelter.enqueue(current)
+            current = self.shelter.dequeue()
+        return pet
 
-        return None
+    def pref_check(self, pref, pet):
+        if pref == "cat":
+            thing_to_dequeue = Cat
+        elif pref == "dog":
+            thing_to_dequeue = Dog
+        return isinstance(thing_to_dequeue, object)
+
+
+# class AnimalShelter:
+#     def __init__(self):
+#         self.cats = Queue()
+#         self.dogs = Queue()
+
+#     def enqueue(self, animal):
+#         if isinstance(animal, Dog):
+#             self.dogs.enqueue(animal)
+#         else:
+#             self.cats.enqueue(animal)
+
+#     def dequeue(self, pref):
+#         if pref == "dog" and self.dogs:
+#             return self.dogs.dequeue()
+#         if pref == "cat" and self.cats:
+#             return self.cats.dequeue()
+
+#         return None
 
 
 class Dog:
